3baf83d9d3232c6cd12e5cdd156bc7e060e66983
hqq777 <48264564+hqq777@users.noreply.github.com>
2019-05-11 21:57:02 -0400

    Update utils.py

--- danger/mrcnn/utils.py
+++ danger/mrcnn/utils.py
@@ -714,7 +714,7 @@ def compute_matches(gt_boxes, gt_class_ids, gt_masks,
 
 def compute_ap(gt_boxes, gt_class_ids, gt_masks,
                pred_boxes, pred_class_ids, pred_scores, pred_masks,
-               iou_threshold=0.5):
+               iou_threshold=0.65):
     """Compute Average Precision at a set IoU threshold (default 0.5).
 
     Returns:
